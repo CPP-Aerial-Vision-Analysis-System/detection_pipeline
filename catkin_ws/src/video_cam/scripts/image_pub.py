6aee44957b824b3a6da3f19b172067b78b700215
joestrada1022 <student.joshuaestrada@gmail.com>
2025-01-02 15:18:59 -0800

    added better logging and testing for cam pub

--- catkin_ws/src/video_cam/scripts/image_pub.py
+++ catkin_ws/src/video_cam/scripts/image_pub.py
@@ -5,20 +5,40 @@ import cv2
 from sensor_msgs.msg import Image
 from cv_bridge import CvBridge
 
-publisherNodeName = 'camera_sensor_publisher'
-topicName = 'image_raw'
 
-rospy.init_node(publisherNodeName, anonymous = True)
-publisher = rospy.Publisher(topicName, Image, queue_size=60)
-rate = rospy.Rate(30)
+class ImagePublisher:
+    def __init__(self):
+        rospy.init_node("cam_pub", anonymous=True)
 
-videoCaptureObject=cv2.VideoCapture(0)
-bridgeObject=CvBridge()
+        self.topic_name = rospy.get_param("~input_topic", "image_raw")
+        self.use_cam = rospy.get_param("~camera", True)
+        print(type(self.use_cam))
+        self.file_path = "/home/car-video.mp4"
 
-while not rospy.is_shutdown():
-    returnValue, capturedFrame = videoCaptureObject.read()
-    if returnValue == True:
-        rospy.loginfo("Video frame captured and published")
-        imageToTransmit = bridgeObject.cv2_to_imgmsg(capturedFrame, encoding='bgr8')
-        publisher.publish(imageToTransmit)
-    rate.sleep()
+        self.publisher = rospy.Publisher(self.topic_name, Image, queue_size=60)
+        self.rate = rospy.Rate(30)
+        self.bridge = CvBridge()
+
+        if self.use_cam:
+            self.video_capture = cv2.VideoCapture(0)
+        else:
+            self.video_capture = cv2.VideoCapture(self.file_path)
+
+    def publish_image(self):
+        while not rospy.is_shutdown():
+            ret, frame = self.video_capture.read()
+            if ret:
+                rospy.loginfo("Video frame captured and published")
+                image_msg = self.bridge.cv2_to_imgmsg(frame, encoding="bgr8")
+                self.publisher.publish(image_msg)
+            else:
+                rospy.logwarn("Failed to capture frame")
+            self.rate.sleep()
+
+
+if __name__ == "__main__":
+    try:
+        image_publisher = ImagePublisher()
+        image_publisher.publish_image()
+    except rospy.ROSInterruptException:
+        pass
